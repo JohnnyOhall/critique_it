a9a424cbe2a79363c723e455bce9e8eb968e5a62
John O'Halloran <john.ohalloran@telus.com>
2023-01-22 20:10:51 -0500

    add skeleton

---
+++ server/resource/testData.js
@@ -0,0 +1,71 @@
+//Database
+const shows = [
+  {
+    id: 1,
+    title: 'The Expanse',
+    img: 'https://www.image.com/test.jpg',
+    seasons: 6,
+    episodes: [
+      {
+        season: 1,
+        episode: 1,
+        title: 'Dulcinea'
+      },
+      {
+        season: 1,
+        episode: 2,
+        title: 'The Big Empty'
+      },
+      {
+        season: 1,
+        episode: 3,
+        title: 'Remember the Cant'
+      },
+      {
+        season: 1,
+        episode: 4,
+        title: 'CQB'
+      },
+      {
+        season: 1,
+        episode: 5,
+        title: 'Back to the Butcher'
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: 'One Tree Hill',
+    img: 'https://www.image.com/test.jpg',
+    seasons: 9,
+    episodes: [
+      {
+        season: 1,
+        episode: 1,
+        title: 'Pilot'
+      },
+      {
+        season: 1,
+        episode: 2,
+        title: 'The Places You Have Come to Fear the Most'
+      },
+      {
+        season: 1,
+        episode: 3,
+        title: 'Are You True?'
+      },
+      {
+        season: 1,
+        episode: 4,
+        title: 'Crash Into You'
+      },
+      {
+        season: 1,
+        episode: 5,
+        title: 'All That You Can\'t Leave Behind'
+      }
+    ]
+  }
+];
+
+module.exports = { shows }
---
+++ server/server.js
@@ -0,0 +1,24 @@
+// load .env data into process.env
+require('dotenv').config();
+
+const express = require( 'express' );
+const morgan = require( 'morgan' );
+
+const app = express();
+const port = process.env.PORT || 54321;
+
+// Middleware
+app.use( morgan( 'dev' ));
+app.use( express.urlencoded({ extended: true }));
+app.use( express.json());
+
+// Separated Routes for each Resource
+const showRoutes = require('./routes/showRoutes');
+
+// Mount all resource routes
+app.use('/shows', showRoutes);
+
+
+app.listen( port, () => {
+  console.log( `app is listening on port ${ port }` );
+})
