dca2419e080f24010598c73ad3772032db0911a2
John O'Halloran <john.ohalloran@telus.com>
2023-02-03 23:27:09 -0500

    add features to add component

--- client/src/components/Main/LeftSide/CritiqueSelectAdd.jsx
+++ client/src/components/Main/LeftSide/CritiqueSelectAdd.jsx
@@ -19,7 +19,7 @@ const CritiqueSelectAdd = props => {
       .then(res => {
         let { name, id, image, rating, summary } = res.data[0].show;
 
-        image = image.medium;
+        image = image.original;
         rating = rating.average;
         summary = removeHTMLTags( summary );
 
@@ -60,42 +60,59 @@ const CritiqueSelectAdd = props => {
       <div className="add">
 
         <div className="show-search-info box">
-          <form className="search" onSubmit={ e => e.preventDefault() }>
-            <input
-              placeholder="Enter Show Name"
-              type="text"
-              value={ search }
-              onChange={ e => setSearch(e.target.value) }
-            />
-            <button onClick={ e => findShow( search ) }>Search</button>
-          </form>
-          <div className="info">
-            <p>Show Title: { show.name }</p>
-            <p>Show ID: { show.id }</p>
-            <p>Rating: { show.rating } / 10</p>
-            <p>Seasons: { show.seasons }</p>
-            <p>Episodes: { show.episodes }</p>
+
+          <div className="top-top">
+            <form className="search" onSubmit={ e => e.preventDefault() }>
+              <input
+                placeholder="Enter Show Name"
+                type="text"
+                value={ search }
+                onChange={ e => setSearch( e.target.value ) }
+              />
+              <button onClick={ e => findShow( search ) }>
+                <img 
+                  src="images/search.png"  
+                  width="32px" 
+                  height="32px"
+                />
+              </button>
+              
+            </form>
+          </div>
+          
+          <div className="top-center">
+              <div>
+                <span className="title">Show Title:</span>
+                <span className="content">{ show.name }</span>
+              </div>
+              <div>
+                <span className="title">Show ID:</span>
+                <span className="content">{ show.id }</span>
+              </div>
+              <div>
+                <span className="title">Rating:</span>
+                <span className="content">{ show.rating } / 10</span>
+              </div>
+              <div>
+                <span className="title">Seasons:</span>
+                <span className="content"> { show.seasons }</span>
+              </div>
+              <div>
+                <span className="title">Episodes:</span>
+                <span className="content">{ show.episodes }</span>
+              </div>
+          </div>
+
+          <div className="top-bottom">
+            <h2>Show Summary</h2>
             <p>{ show.summary }</p>
           </div>
+
         </div>
+      
 
         <div className="show-image box">
           <img src={ show.image } />
-        </div>
-
-        <div className="show-buttons box">
-          <img
-            className="select-button"
-            src="images/add.png"
-            alt="Select"
-            onClick={ () => selectShow(show) }
-          />
-          <img
-            className="close-button"
-            src="images/close.png"
-            alt="Close"
-            onClick={ props.onClose }
-          />
         </div>
 
         <div className="show-stats box">
@@ -105,6 +122,30 @@ const CritiqueSelectAdd = props => {
           <p>Average episodes critiqued:</p>
           <p>Times searched:</p>
         </div>
+
+        <div className="show-buttons box">
+          <div className="add-button">
+          <img
+            className="select-button"
+            src="images/add.png"
+            alt="Select"
+            onClick={ () => selectShow(show) }
+          /><h2>add</h2>
+          </div>
+          <div className="close-button">
+          <img
+            className="close-button"
+            src="images/cancel.png"
+            alt="Close"
+            width="64px" height="64px"
+            onClick={ props.onClose }
+          /> <h2>close</h2>
+          </div>
+          
+          
+        </div>
+
+        
 
       </div>
 
