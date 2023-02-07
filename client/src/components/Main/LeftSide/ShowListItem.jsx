909e5009095a3766707b5c2447f250e5beff2d66
Jacquiiii <jacqui.koroll@telus.com>
2023-02-06 19:18:47 -0800

    add logic for add/edit button when show name is displayed

--- client/src/components/Main/LeftSide/ShowListItem.jsx
+++ client/src/components/Main/LeftSide/ShowListItem.jsx
@@ -14,26 +14,31 @@ const ShowListItem = props => {
 
   const findShow = () => {
     let showData;
+    console.log('episode (should match target): ', episode)
 
     axios.get(`http://api.tvmaze.com/shows/${props.show_id}/seasons`)
-    .then(res => {
+      .then(res => {
+        showData = {
+          seasonLength: res.data.length,
+          seasonId: res.data[ season - 1 ].id
+        };
 
-      showData = {
-        seasonLength: res.data.length,
-        seasonId: res.data[ season -1 ].id
-      };
+        return axios.get(`http://api.tvmaze.com/seasons/${showData.seasonId}/episodes`)
+      })
+      .then(res => {
+        showData.episodeLength = res.data.length;
+        showData.episode = res.data[ episode - 1 ];
 
-      return axios.get(`http://api.tvmaze.com/seasons/${showData.seasonId}/episodes`)
-    })
-    .then(res => {
-      showData.episodeLength = res.data.length;
-      showData.episode = res.data[ episode -1 ];
-
-      setSeasonMax( showData.seasonLength );
-      setEpisodeMax( showData.episodeLength );
-      setEpisodeInfo( showData.episode );
-    })
-    .catch(err => console.error(err));
+        return axios.get( `/pages/${ showData.episode.id }` );
+      })
+      .then(res => {
+        const exists = res.data.pageData[0] ? true : false;
+        console.log('showdata.episode.name: ', showData.episode.name)
+        setSeasonMax( showData.seasonLength );
+        setEpisodeMax( showData.episodeLength );
+        setEpisodeInfo( { ...showData.episode, exists } );
+      })
+      .catch(err => console.error(err));
   };
 
   const assignSeason = value => {
@@ -43,11 +48,12 @@ const ShowListItem = props => {
   };
 
   const assignEpisode = value => {
+    console.log('value', value);
     setEpisode( value );
     findShow();
   };
 
-  console.log('ep info: ', episodeInfo)
+  console.log('episode: ', episodeInfo.name)
 
   return (
     <li className="show-item">
@@ -96,7 +102,10 @@ const ShowListItem = props => {
             />
           </div>
 
-          <div><span>{ episodeInfo.name }</span></div>
+          <div>
+            <span>{ episodeInfo.name }{ episodeInfo.exists && "✅" }</span>
+            { episodeInfo.exists ? <button>Edit</button> : <button>Add</button> }
+          </div>
         </div>
       }
     </li>
