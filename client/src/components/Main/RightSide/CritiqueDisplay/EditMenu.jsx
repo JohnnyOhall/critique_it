bc5a8212097e1b164007eaee6358d9e4769e13c1
Jacquiiii <jacqui.koroll@telus.com>
2023-02-08 20:34:57 -0800

    add styling to Edit component

--- client/src/components/Main/RightSide/CritiqueDisplay/EditMenu.jsx
+++ client/src/components/Main/RightSide/CritiqueDisplay/EditMenu.jsx
@@ -20,43 +20,43 @@ const EditMenu = props => {
     const menuButton = "https://freeiconshop.com/wp-content/uploads/edd/menu-outline.png"
 
     return (
-        <>
-            <Menu menuButton={<MenuButton className="menu-button"><img height="50px" width="50px" src={menuButton}/></MenuButton>}>
-                <MenuHeader>Page Options</MenuHeader>
+      <>
+        <Menu menuButton={<MenuButton className="menu-button"><img height="40px" width="40px" src={menuButton}/></MenuButton>}>
+          <MenuHeader>Page Options</MenuHeader>
 
-                <SubMenu label="Text color">
-                    <MenuRadioGroup
-                        value={textColor}
-                        onRadioChange={e => setTextColor(e.value)}>
-                        <MenuItem type="radio" value={'red'}>Red</MenuItem>
-                        <MenuItem type="radio" value={'green'}>Green</MenuItem>
-                        <MenuItem type="radio" value={'blue'}>Blue</MenuItem>
-                    </MenuRadioGroup>
-                </SubMenu>
+          <SubMenu label="Text color">
+            <MenuRadioGroup
+              value={textColor}
+              onRadioChange={e => setTextColor(e.value)}>
+              <MenuItem type="radio" value={'red'}>Red</MenuItem>
+              <MenuItem type="radio" value={'green'}>Green</MenuItem>
+              <MenuItem type="radio" value={'blue'}>Blue</MenuItem>
+            </MenuRadioGroup>
+          </SubMenu>
 
-                <SubMenu label="Text style">
-                    <MenuItem type="checkbox" checked={isBold}
-                        onClick={e => setBold(e.checked)}>
-                        Bold
-                    </MenuItem>
-                    <MenuItem type="checkbox" checked={isItalic}
-                        onClick={e => setItalic(e.checked)}>
-                        Italic
-                    </MenuItem>
-                    <MenuItem type="checkbox" checked={isUnderline}
-                        onClick={e => setUnderline(e.checked)}>
-                        Underline
-                    </MenuItem>
-                </SubMenu>
-            </Menu>
+          <SubMenu label="Text style">
+            <MenuItem type="checkbox" checked={isBold}
+              onClick={e => setBold(e.checked)}>
+              Bold
+            </MenuItem>
+            <MenuItem type="checkbox" checked={isItalic}
+              onClick={e => setItalic(e.checked)}>
+              Italic
+            </MenuItem>
+            <MenuItem type="checkbox" checked={isUnderline}
+              onClick={e => setUnderline(e.checked)}>
+              Underline
+            </MenuItem>
+          </SubMenu>
+        </Menu>
 
-            <div className="sample-text" style={{
-                color: textColor,
-                fontWeight: isBold ? 'bold' : 'initial',
-                fontStyle: isItalic ? 'italic' : 'initial',
-                textDecoration: isUnderline ? 'underline' : 'initial'
-            }}>Sample text</div>
-        </>
+        <div className="sample-text" style={{
+          color: textColor,
+          fontWeight: isBold ? 'bold' : 'initial',
+          fontStyle: isItalic ? 'italic' : 'initial',
+          textDecoration: isUnderline ? 'underline' : 'initial'
+        }}>Sample text</div>
+      </>
     );
 }
 
